4130e2339fe4b1286ce3b7d494f546dc70c9cb88
Nazwa Allysa <nazwa.allysa@ui.ac.id>
2024-02-20 08:47:42 +0700

    tutorial 2 selesai

--- main/views.py
+++ main/views.py
@@ -1,9 +1,42 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from django.shortcuts import render, redirect   
+from main.forms import BookForm
+from main.models import Book
 
 def show_main(request):
+    books = Book.objects.all()
+
     context = {
         'name': 'Nazwa Allysa',
-        'class': 'PBP A'
+        'class': 'PBP A',
+        'books': books
     }
 
     return render(request, "main.html", context)
+
+def create_book(request):
+    form = BookForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_book.html", context)
+
+def show_xml(request):
+    data = Book.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Book.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, book_id):
+    data = Book.objects.filter(pk=book_id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, book_id):
+    data = Book.objects.filter(pk=book_id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
