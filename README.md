a5d69c4d70de04b5131aadb15622852693fe8df0
Nazwa Allysa <nazwa.allysa@ui.ac.id>
2024-02-27 08:55:41 +0700

    tutorial 4 selesai

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Nama : Nazwa Allysa
 NPM : 2206083672
 
 Kelas : PBP A
+
--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from main.views import show_main, create_book, show_xml, show_json, show_xml_by_
 from main.views import register
 from main.views import login_user
 from main.views import logout_user
+from main.views import edit_book
+from main.views import delete_book
 
 app_name = 'main'
 
@@ -16,4 +18,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('edit-book/<int:book_id>', edit_book, name='edit_book'),
+    path('delete/<int:book_id>', delete_book, name='delete_book'),
 ]
